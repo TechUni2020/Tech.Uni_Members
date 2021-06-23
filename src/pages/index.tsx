f2b3f3cb46a37baf9c2afd45f5f8d77ed7f8d1dc
ide Shouhi <shouhi0922@icloud.com>
2021-06-23 14:20:26 +0900

    Tailwind CSS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import styles from "../../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -11,8 +11,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main className="space-y-7 color-">
+        <h1 className="font-bold">
           Welcome to <a href="https://nextjs.org">Next.js! + TypeScript!</a>
         </h1>
 
