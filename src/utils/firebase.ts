fb94db1a2b05ead7e5ac3bb52099371be134f1c0
井手翔陽 <shouhi605@gmail.com>
2022-03-01 14:40:54 +0900

    :closed_lock_with_key: envファイルの読み込み修正

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -5,13 +5,12 @@ import "firebase/storage";
 import firebase from "firebase/app";
 
 const config = {
-  apiKey: process.env.FIREBASE_KEY,
-  authDomain: process.env.FIREBASE_DOMAIN,
-  databeseURL: process.env.FIREBASE_DATABASE,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_SENDER_ID,
-  appId: process.env.FIREBASE_APPID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // initializeを複数回走らせない
